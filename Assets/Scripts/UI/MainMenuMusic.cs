eef52c992ea98dd9db8911306ed45b69f2053886
D-nop22 <pangpond21548@gmail.com>
2026-03-01 05:05:32 +0700

    music

---
+++ Assets/Scripts/UI/MainMenuMusic.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MainMenuMusic : MonoBehaviour
+{
+    [SerializeField] AudioClip musicClip;
+    [SerializeField] [Range(0f,1f)] float volume = 1f;
+    [SerializeField] bool loop = true;
+    [SerializeField] bool playOnStart = true;
+
+    AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        source.playOnAwake = false;
+        source.loop = loop;
+        source.volume = volume;
+        if (musicClip != null)
+            source.clip = musicClip;
+    }
+
+    void Start()
+    {
+        if (playOnStart && source.clip != null)
+            source.Play();
+    }
+
+    // Optional helper to stop when starting game
+    public void StopMusic()
+    {
+        if (source.isPlaying) source.Stop();
+    }
+}
